c8ddcf7ac217dccd2685f958d7ae03f33025e4d5
KaYu <93873934+kayusol@users.noreply.github.com>
2026-03-11 16:15:43 +0800

    feat: ObjectOwnership ERC721 contract

---
+++ contracts/ObjectOwnership.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title ObjectOwnership
+ * @dev ERC721 base for land and apostle NFTs. 
+ *      Authorized operators (other game contracts) can mint/burn.
+ */
+contract ObjectOwnership is ERC721, Ownable {
+
+    uint256 private _tokenIdCounter;
+    mapping(address => bool) public operators;
+
+    event OperatorSet(address indexed operator, bool enabled);
+
+    modifier onlyOperator() {
+        require(operators[msg.sender] || msg.sender == owner(), "Not operator");
+        _;
+    }
+
+    constructor() ERC721("Evolution Land Object", "ELO") Ownable(msg.sender) {}
+
+    function setOperator(address _operator, bool _enabled) external onlyOwner {
+        operators[_operator] = _enabled;
+        emit OperatorSet(_operator, _enabled);
+    }
+
+    function mint(address _to, uint256 _tokenId) external onlyOperator {
+        _safeMint(_to, _tokenId);
+    }
+
+    function burn(uint256 _tokenId) external onlyOperator {
+        _burn(_tokenId);
+    }
+
+    // Allow operator transfers (needed for auction contracts)
+    function transferFrom(address from, address to, uint256 tokenId) public override {
+        if (operators[msg.sender]) {
+            _transfer(from, to, tokenId);
+        } else {
+            super.transferFrom(from, to, tokenId);
+        }
+    }
+
+    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override {
+        if (operators[msg.sender]) {
+            _safeTransfer(from, to, tokenId, data);
+        } else {
+            super.safeTransferFrom(from, to, tokenId, data);
+        }
+    }
+}
