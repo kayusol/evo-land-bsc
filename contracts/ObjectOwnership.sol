4113403040641c3227018d86930461ecda0e2e41
KaYu <93873934+kayusol@users.noreply.github.com>
2026-03-11 20:16:14 +0800

    fix: ObjectOwnership - OZ v5 safeTransfer compatibility, inline ERC721Receiver check

--- contracts/ObjectOwnership.sol
+++ contracts/ObjectOwnership.sol
@@ -6,9 +6,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title ObjectOwnership
- * @dev ERC721 base for land and apostle NFTs.
- *      Authorized operators (other game contracts) can mint/burn/transfer.
- *      Compatible with OpenZeppelin v5.
+ * @dev ERC721 for all game NFTs (lands, apostles). OZ v5 compatible.
+ *      Operator contracts (LandBase, ClockAuction) can mint/transfer without approval.
  */
 contract ObjectOwnership is ERC721, Ownable {
 
@@ -36,10 +35,7 @@ contract ObjectOwnership is ERC721, Ownable {
         _burn(_tokenId);
     }
 
-    /**
-     * @dev Override transferFrom to allow authorized operators to transfer on behalf.
-     *      OZ v5 compatible - checks approval internally via _isAuthorized.
-     */
+    // Allow operator contracts to transfer without needing explicit approval
     function transferFrom(address from, address to, uint256 tokenId) public override {
         if (operators[msg.sender]) {
             _transfer(from, to, tokenId);
@@ -48,14 +44,40 @@ contract ObjectOwnership is ERC721, Ownable {
         }
     }
 
-    /**
-     * @dev Override safeTransferFrom (4-arg version). OZ v5 signature unchanged.
-     */
-    function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override {
+    // OZ v5: safeTransferFrom calls _checkOnERC721Received internally after _transfer
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) public override {
         if (operators[msg.sender]) {
-            _safeTransfer(from, to, tokenId, data);
+            _transfer(from, to, tokenId);
+            _checkOnERC721Received(msg.sender, from, to, tokenId, data);
         } else {
             super.safeTransferFrom(from, to, tokenId, data);
         }
     }
+
+    function _checkOnERC721Received(
+        address operator_,
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory data
+    ) internal {
+        if (to.code.length > 0) {
+            try IERC721Receiver(to).onERC721Received(operator_, from, tokenId, data)
+                returns (bytes4 retval) {
+                require(retval == IERC721Receiver.onERC721Received.selector,
+                    "ERC721: transfer to non ERC721Receiver");
+            } catch {
+                revert("ERC721: transfer to non ERC721Receiver");
+            }
+        }
+    }
+}
+
+interface IERC721Receiver {
+    function onERC721Received(address, address, uint256, bytes calldata) external returns (bytes4);
 }
